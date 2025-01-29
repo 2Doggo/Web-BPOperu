8005bf4f289bbc9b9fb7feedfb95c7128d762b5c
doggo <d.a.novoacisternas@gmail.com>
2025-01-29 11:53:37 -0300

    Modificado el Nosotros, agregada una biblioteca, comentados componentes no usados

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import Header from './components/Header';  // Importamos el Header
-import Home from './pages/Home';  // Importamos Home 
-import WhatsappFloat from './components/ContactFloat'; //Importartamos Whatsapp flotante
-import './App.css';
+import Header from "./components/Header"; // Importamos el Header
+import Home from "./pages/Home"; // Importamos Home
+import WhatsappFloat from "./components/ContactFloat"; //Importartamos Whatsapp flotante
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Header />  
+      <Header />
       <Home />
-      <WhatsappFloat />
+      {/* <WhatsappFloat /> */}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,61 @@
 import React, { useState, useEffect } from "react";
+import { X } from "lucide-react"; // Importamos el ícono de cerrar
 import "./Components.css"; // Importamos los estilos de Header
+
+const CompanyInfoOverlay = ({ onClose }) => {
+  return (
+    <div className="company-info-overlay">
+      <button className="close-button" onClick={onClose}>
+        <X size={24} />
+      </button>
+
+      <div className="company-info-content">
+        <section className="info-section">
+          <h2>Nosotros</h2>
+          <div className="blue-line"></div>
+          <p className="main-description">
+            Impulsando el futuro digital de tus negocios
+          </p>
+          <p className="description-text">
+            BPO IDOK es una empresa líder en la optimización de procesos de
+            gestión, identificación digital y firma electrónica. Con una sólida
+            presencia en Latinoamérica, ofrecemos soluciones tecnológicas
+            adaptadas a las necesidades de nuestros clientes, garantizando
+            seguridad, eficiencia y confianza en todos sus procesos
+          </p>
+          <p className="description-text">
+            Nos enorgullece ofrecer nuestras soluciones en Perú, un país clave
+            en la transformación digital de Latinoamérica. Con nuestra presencia
+            y acreditación local, ayudamos a las empresas peruanas a optimizar
+            sus procesos de forma segura, confiable y eficiente.
+          </p>
+        </section>
+
+        <section className="info-section">
+          <h2>Misión</h2>
+          <div className="blue-line"></div>
+          <p className="description-text">
+            Ayudar a nuestros clientes a modelar, implementar, gestionar y
+            optimizar sus procesos de negocio y operativos, relacionados con
+            documentación y firma electrónica que permitan generar valor con
+            confianza y seguridad.
+          </p>
+        </section>
+
+        <section className="info-section">
+          <h2>Visión</h2>
+          <div className="blue-line"></div>
+          <p className="description-text">
+            Ser una empresa posicionada en el mercado por su profesionalismo,
+            confiabilidad, seguridad y permanente innovación, aportando
+            soluciones efectivas, valor sustentable y satisfacción a las
+            necesidades de los clientes.
+          </p>
+        </section>
+      </div>
+    </div>
+  );
+};
 
 const Header = () => {
   const [scrolled, setScrolled] = useState(false); // Estado para saber si hemos hecho scroll
@@ -7,7 +63,8 @@ const Header = () => {
   const [isMenuVisible, setIsMenuVisible] = useState(false); // Estado para controlar la visibilidad del menu
   const [activeService, setActiveService] = useState(null); // Para gestionar el servicio activo
   const [activeMenuItem, setActiveMenuItem] = useState(null);
-  const [isCountriesVisible, setIsCountriesVisible] = useState(false); // Estado para manejar la visibilidad de la lista de países
+  const [showCompanyInfo, setShowCompanyInfo] = useState(false);
+  /*const [isCountriesVisible, setIsCountriesVisible] = useState(false); // Estado para manejar la visibilidad de la lista de países*/
 
   // Datos de los servicios
 
@@ -27,8 +84,6 @@ const Header = () => {
               <h4>Para ciudadanos</h4>
               <a>Persona natural</a>
             </div>
-          </div>
-          <div className="columns-container">
             {/* Columna 2 */}
             <div className="column beneficios">
               <h4>Para empresas</h4>
@@ -36,8 +91,6 @@ const Header = () => {
               <a>Empleado</a>
               <a>Agente automatizado</a>
             </div>
-          </div>
-          <div className="columns-container">
             {/* Columna 3 */}
             <div className="column beneficios">
               <h4>Para profesionales</h4>
@@ -62,6 +115,9 @@ const Header = () => {
             {/* Columna 1 */}
             <div className="column beneficios"></div>
           </div>
+          <a href="#vermas1" className="ver-mas-link">
+            Ver más <span>&#8594;</span> {/* Flecha hacia la derecha */}
+          </a>
         </>
       ),
     },
@@ -70,8 +126,7 @@ const Header = () => {
       name: "Sellos de tiempo",
       description: (
         <>
-          <div className="columns-container">
-          </div>
+          <div className="columns-container"></div>
           <a href="#vermas" className="ver-mas-link">
             Ver más <span>&#8594;</span>
           </a>
@@ -97,6 +152,9 @@ const Header = () => {
               <a>Firma remota</a>
             </div>
           </div>
+          <a href="#vermas1" className="ver-mas-link">
+            Ver más <span>&#8594;</span> {/* Flecha hacia la derecha */}
+          </a>
         </>
       ),
     },
@@ -121,6 +179,9 @@ const Header = () => {
               <a>Politica de Privacidad</a>
             </div>
           </div>
+          <a href="#vermas1" className="ver-mas-link">
+            Ver más <span>&#8594;</span> {/* Flecha hacia la derecha */}
+          </a>
         </>
       ),
     },
@@ -130,9 +191,11 @@ const Header = () => {
       description: (
         <>
           <div className="columns-container">
-            <div className="column beneficios">
-            </div>
+            <div className="column beneficios"></div>
           </div>
+          <a href="#vermas1" className="ver-mas-link">
+            Ver más <span>&#8594;</span> {/* Flecha hacia la derecha */}
+          </a>
         </>
       ),
     },
@@ -141,8 +204,7 @@ const Header = () => {
       name: "Procesos de negocio",
       description: (
         <>
-          <div className="columns-container">
-          </div>
+          <div className="columns-container"></div>
           <a href="#vermas" className="ver-mas-link">
             Ver más <span>&#8594;</span>
           </a>
@@ -155,9 +217,11 @@ const Header = () => {
       description: (
         <>
           <div className="columns-container">
-            <div className="column beneficios">
-            </div>
+            <div className="column beneficios"></div>
           </div>
+          <a href="#vermas1" className="ver-mas-link">
+            Ver más <span>&#8594;</span> {/* Flecha hacia la derecha */}
+          </a>
         </>
       ),
     },
@@ -166,8 +230,7 @@ const Header = () => {
       name: "Gestor documental",
       description: (
         <>
-          <div className="columns-container">
-          </div>
+          <div className="columns-container"></div>
           <a href="#vermas" className="ver-mas-link">
             Ver más <span>&#8594;</span>
           </a>
@@ -179,8 +242,10 @@ const Header = () => {
       name: "Noticias",
       description: (
         <>
-          <div className="columns-container">
-          </div>
+          <div className="columns-container"></div>
+          <a href="#vermas1" className="ver-mas-link">
+            Ver más <span>&#8594;</span> {/* Flecha hacia la derecha */}
+          </a>
         </>
       ),
     },
@@ -189,8 +254,10 @@ const Header = () => {
       name: "Contáctanos",
       description: (
         <>
-          <div className="columns-container">
-          </div>
+          <div className="columns-container"></div>
+          <a href="#vermas1" className="ver-mas-link">
+            Ver más <span>&#8594;</span> {/* Flecha hacia la derecha */}
+          </a>
         </>
       ),
     },
@@ -223,9 +290,12 @@ const Header = () => {
 
   // Función para manejar el mouse enter
   const handleMouseEnter = (menuId) => {
-    setActiveMenuItem(menuId); // Establece el id del menú que está activo
-    setIsMenuVisible(true);
-    setSelectedService(null);
+    if (menuId !== 1 || !scrolled) {
+      // No mostrar el menú desplegable para "Nosotros"
+      setActiveMenuItem(menuId);
+      setIsMenuVisible(true);
+      setSelectedService(null);
+    }
   };
 
   const handleMouseLeave = () => {
@@ -233,60 +303,66 @@ const Header = () => {
   };
 
   // Función para alternar la visibilidad de la lista de países
-  const toggleCountriesList = () => {
+  /*  const toggleCountriesList = () => {
     setIsCountriesVisible(!isCountriesVisible);
   };
-
+*/
+  const handleNosotrosClick = (e) => {
+    e.preventDefault();
+    setShowCompanyInfo(true);
+    setIsMenuVisible(false);
+  };
   return (
-    <header
-      className={`header ${scrolled ? "scrolled" : ""} ${
-        isMenuVisible ? "visible" : ""
-      }`}
-      onMouseLeave={handleMouseLeave}
-    >
-      <div className="logo-container">
-        {/* Cambiamos la imagen según el estado del scroll */}
-        <img src={"/idok.png"} alt="Logo" className="logo" />
-      </div>
+    <>
+      <header
+        className={`header ${scrolled ? "scrolled" : ""} ${
+          isMenuVisible ? "visible" : ""
+        }`}
+        onMouseLeave={handleMouseLeave}
+      >
+        <div className="logo-container">
+          {/* Cambiamos la imagen según el estado del scroll */}
+          <img src={"/idok.png"} alt="Logo" className="logo" />
+        </div>
 
-      {scrolled && ( // Solo mostramos el menú cuando hay scroll
-        <nav className="navbar">
-          <ul>
-            <li
-              className={`nav-item ${activeMenuItem === 1 ? "active" : ""}`}
-              onMouseEnter={() => handleMouseEnter(1)}
-            >
-              <a href="#servicios">Nosotros</a>
-            </li>
-            <li
-              className={`nav-item ${activeMenuItem === 2 ? "active" : ""}`}
-              onMouseEnter={() => handleMouseEnter(2)}
-            >
-              <a href="#servicios">Servicios digitales</a>
-            </li>
-            <li
-              className={`nav-item ${activeMenuItem === 3 ? "active" : ""}`}
-              onMouseEnter={() => handleMouseEnter(3)}
-            >
-              <a href="#servicios">Soluciones</a>
-            </li>
-            <li
-              className={`nav-item ${activeMenuItem === 4 ? "active" : ""}`}
-              onMouseEnter={() => handleMouseEnter(4)}
-            >
-              <a href="#paises">Más</a>
-            </li>
-          </ul>
-        </nav>
-      )}
-      {scrolled && (
+        {scrolled && (
+          <nav className="navbar">
+            <ul>
+              <li
+                className={`nav-item ${activeMenuItem === 1 ? "active" : ""}`}
+                onClick={handleNosotrosClick}
+                onMouseEnter={() => handleMouseEnter(1)}
+              >
+                <a href="#nosotros">Nosotros</a>
+              </li>
+              <li
+                className={`nav-item ${activeMenuItem === 2 ? "active" : ""}`}
+                onMouseEnter={() => handleMouseEnter(2)}
+              >
+                <a href="#servicios">Servicios digitales</a>
+              </li>
+              <li
+                className={`nav-item ${activeMenuItem === 3 ? "active" : ""}`}
+                onMouseEnter={() => handleMouseEnter(3)}
+              >
+                <a href="#servicios">Soluciones</a>
+              </li>
+              <li
+                className={`nav-item ${activeMenuItem === 4 ? "active" : ""}`}
+                onMouseEnter={() => handleMouseEnter(4)}
+              >
+                <a href="#servicios">Más</a>
+              </li>
+            </ul>
+          </nav>
+        )}
+
+        {/*scrolled && (
         <div onClick={toggleCountriesList}>
           <div className="pais-section">
-            <i className="fas fa-globe"></i> {/* Icono del planeta */}
+            <i className="fas fa-globe"></i>
             <i className="fas fa-chevron-down"></i>{" "}
-            {/* Icono de la flecha hacia abajo */}
           </div>
-          {/* Solo mostramos la lista si isCountriesVisible es true */}
           {isCountriesVisible && (
             <ul className="countries-list">
               <li>Argentina</li>
@@ -297,42 +373,47 @@ const Header = () => {
             </ul>
           )}
         </div>
-      )}
+      )*/}
 
-      {/* Contenedor para las dos columnas */}
-      <div className="container-menu">
-        <div className="option-menu">
-          <ul>
-            {services.map((service) => (
-              <li
-                className={`${service.id} ${
-                  activeMenuItem === service.id ? "active" : ""
-                }`}
-                key={service.id}
-              >
-                <a
-                  onClick={() => handleServiceClick(service)}
-                  className={activeService === service.name ? "active" : ""} // Agrega la clase 'active'
+        {/* Contenedor para las dos columnas */}
+        <div className="container-menu">
+          <div className="option-menu">
+            <ul>
+              {services.map((service) => (
+                <li
+                  className={`${service.id} ${
+                    activeMenuItem === service.id ? "active" : ""
+                  }`}
+                  key={service.id}
                 >
-                  {service.name}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
+                  <a
+                    onClick={() => handleServiceClick(service)}
+                    className={activeService === service.name ? "active" : ""} // Agrega la clase 'active'
+                  >
+                    {service.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
 
-        <div className="content-option">
-          {selectedService ? (
-            <>
-              <h3>{selectedService.name}</h3>
-              <p>{selectedService.description}</p>
-            </>
-          ) : (
-            <p>Selecciona una opción para ver más detalles.</p>
-          )}
+          <div className="content-option">
+            {selectedService ? (
+              <>
+                <h3>{selectedService.name}</h3>
+                <p>{selectedService.description}</p>
+              </>
+            ) : (
+              <p>Selecciona una opción para ver más detalles.</p>
+            )}
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+
+      {showCompanyInfo && (
+        <CompanyInfoOverlay onClose={() => setShowCompanyInfo(false)} />
+      )}
+    </>
   );
 };
 
